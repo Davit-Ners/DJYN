ccb979e7b0419212bc557bb209be5fc839dec0f3
Davit <davit.ners02@gmail.com>
2025-04-08 22:24:10 +0200

    Ajout meilleur logo

--- src/containers/header/header.tsx
+++ src/containers/header/header.tsx
@@ -5,7 +5,7 @@ import MediaLinks from '@/components/mediaLinks/mediaLinks';
 export default function Header() {
     return (
         <header className={style.header}>
-            <Image className={style.logo} alt='Logo du groupe Djyn' src='/logo.png' width={400} height={400}/>
+            <Image className={style.logo} alt='Logo du groupe Djyn' src='/logo1.png' width={300} height={300}/>
             <div className={style.desc}>
                 <p>Modern Metal Duo - Made In Brussels</p>
                 <MediaLinks />
