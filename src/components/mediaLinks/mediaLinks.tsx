868af6453796db03019a279f75f9105878b9aebf
Davit <davit.ners02@gmail.com>
2025-04-08 22:17:36 +0200

    Refacto pour ajotuer liens dans object social media

--- src/components/mediaLinks/mediaLinks.tsx
+++ src/components/mediaLinks/mediaLinks.tsx
@@ -1,18 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import style from './mediaLinks.module.css';
+import { socialMedias, SocialMedias } from "@/services/socialMedias";
 
 export default function MediaLinks() {
-
-    const socialIcon = ['instagram', 'facebook', 'tiktok', 'youtube', 'spotify']
     
     return (
         <div className={style.linkContainer}>
-            {socialIcon.map(icon => (
-                <Link target="_blank" key={icon} href=''>
+            {socialMedias.map(({name, url}) => (
+                <Link target="_blank" key={name} href={url}>
                     <Image
-                        alt={`Logo de ${icon}`}
-                        src={`/${icon}.svg`}
+                        alt={`Logo de ${name}`}
+                        src={`/${name}.svg`}
                         width={45}
                         height={45}
                         className={style.test}
---
+++ src/services/socialMedias.ts
@@ -0,0 +1,11 @@
+export type SocialMedias = {
+    name: string;
+    url: string;
+}
+export const socialMedias: SocialMedias[] = [
+    { name: 'instagram', url: 'https://www.instagram.com/djyn_band' },
+    { name: 'facebook', url: 'https://www.facebook.com/profile.php?id=61552933106919' },
+    { name: 'tiktok', url: 'https://www.tiktok.com/@djyn_band' },
+    { name: 'youtube', url: 'https://www.youtube.com/@DjynBand' },
+    { name: 'spotify', url: 'https://open.spotify.com/intl-fr/artist/3ONdP64bOHEjP8s2PdpRLR' },
+];
