902ca66ddd4bd8fe289a4318f9f72ea7b656217e
Davit <davit.ners02@gmail.com>
2025-04-23 09:47:06 +0200

    Add about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import SpotifyIntegrator from '@/components/spotifyIntegrator/spotifyIntegrator';
 import style from './homePage.module.css';
 import Image from "next/image";
+import AboutSection from '@/components/aboutSection/aboutSection';
 
 export default function Home() {
     return (
         <div className={style.pageContainer}>
-            <div className={style.spotify}>
-                <SpotifyIntegrator />
+            <div className={style.leftPage}>
+                <div className={style.spotify}>
+                    <SpotifyIntegrator />
+                </div>
+
+                <div className={style.aboutContainer}>
+                    <AboutSection />
+                </div>
             </div>
         </div>
     );
