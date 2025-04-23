16ccc046e480932a2ecc013849272741bf563262
Davit <davit.ners02@gmail.com>
2025-04-23 10:05:49 +0200

    Reagencenement style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,19 @@ export default function Home() {
     return (
         <div className={style.pageContainer}>
             <div className={style.leftPage}>
+                <h2 className={style.sectionTitle}>🎧 Listen on Spotify</h2>
                 <div className={style.spotify}>
                     <SpotifyIntegrator />
                 </div>
 
+                <h2 className={style.sectionTitle}>🧬 About DJYN</h2>
                 <div className={style.aboutContainer}>
                     <AboutSection />
                 </div>
             </div>
 
             <div className={style.rightPage}>
+                <h2 className={style.sectionTitle}>🎥 Official Music Video</h2>
                 <div className={style.youtube}>
                     <YoutubeIntegrator />
                 </div>
--- src/components/aboutSection/aboutSection.tsx
+++ src/components/aboutSection/aboutSection.tsx
@@ -1,7 +1,6 @@
 export default function AboutSection() {
     return (
         <>
-            <h2>ABOUT</h2>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo, facilis eius dicta deleniti, quos reprehenderit laudantium aliquam, ex atque mollitia vitae nulla laboriosam modi consectetur esse similique deserunt commodi quasi.
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ducimus unde quo quidem ea reprehenderit! Temporibus rem illo a debitis at id, non modi laboriosam harum consequuntur in necessitatibus odit.
             </p>
